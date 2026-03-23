290474977d765ccd24b7ed2d0c075f325d091dc3
Ranju Raveendran <39932642+ranjurave@users.noreply.github.com>
2026-03-23 15:53:53 +1300

    Initial commit

---
+++ Assets/Scripts/RobotMovement.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class RobotMovement : MonoBehaviour
+{
+    CharacterController robotController;
+    [SerializeField] InputActionAsset inputActions;
+    InputActionMap actionMap;
+    InputAction moveAction;
+    float currentVelocity; // For smooth rotation pass to Mathf.SmoothDampAngle
+    //Vector2 moveInput;
+    // Vector3 moveDirection;
+
+    [SerializeField] float speed = 5f;
+    [SerializeField] float rotationSmoothTime = 0.1f;
+
+    void Awake() {
+        actionMap = inputActions.FindActionMap("Player");
+        moveAction = actionMap.FindAction("Move");
+    }
+
+    void OnEnable() {
+        actionMap.Enable();
+    }
+
+    void OnDisable() {
+        actionMap.Disable();
+    }
+
+    void Start() {
+        robotController = GetComponent<CharacterController>();
+    }
+
+    void FixedUpdate() {
+        Vector2 moveInput = moveAction.ReadValue<Vector2>();
+        Vector3 moveDirection = new Vector3(moveInput.x, 0, moveInput.y);   
+
+        float targetAngle = Mathf.Atan2(moveDirection.x, moveDirection.z) * Mathf.Rad2Deg;
+        float smoothAngle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref currentVelocity, rotationSmoothTime);
+        Debug.Log(smoothAngle);
+        transform.rotation = Quaternion.Euler(0, smoothAngle, 0);
+
+        robotController.Move(moveDirection * Time.deltaTime * speed);
+    }
+}
